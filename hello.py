cdc39bd0644e46034412305b0707a0a4d97d38ce
mikethedefense <41405040+mikethedefense@users.noreply.github.com>
2018-07-18 21:24:32 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Hi this is my first repository")
+
+i = int(input("Enter your number!: "))
+
+if i < 10:
+    print("i is smaller than 10!")
+elif i == 10:
+    print("i is equal to 10")
+else:
+    print("i is greater than 10")
